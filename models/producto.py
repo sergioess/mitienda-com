5e15570cc1c4d851a53ec5af0981e92bcb0672bb
williamv84 <willillo7@hotmail.com>
2021-09-23 22:38:01 -0500

    enlace a html

--- models/producto.py
+++ models/producto.py
@@ -11,13 +11,43 @@ class Producto(database.Model):
     costo = database.Column(database.Float, nullable=False)
     precio_venta = database.Column(database.Float, nullable=False)
     imagen = database.Column(database.String, nullable=True)
-    #id_tienda = database.Column(database.Integer, nullable=False)
-    # categoria_id = database.Column(database.Integer, database.Foreignkey("categoria.id"))
+    # tienda_id = database.Column(database.Integer, nullable=False)
+    # categoria_id = database.Column(database.Integer, database.Foreignkey("Producto.id"))
     activo = database.Column(database.Integer, nullable=False)
 
+    def __init__(self, id):
+        self.id = id
+
+
+
+    def __init__(self, id, nombre, referencia, costo, precio_venta):
+        self.id = id
+        self.nombre = nombre
+        self.referencia = referencia
+        self.costo = costo
+        self.precio_venta = precio_venta
+    
     @staticmethod
     def get_all():
         return Producto.query.all()
+
+    def update(self):
+        productoActualiza = Producto.query.filter_by(id=self.id).first()
+        productoActualiza.nombre = self.nombre
+        productoActualiza.referencia = self.referencia
+        productoActualiza.costo = self.costo
+        productoActualiza.precio_venta = self.precio_venta
+        database.session.commit()
+        return productoActualiza
+
+    def delete(self):
+        #print(self.id)
+        productoActualiza = Producto.query.filter_by(id=self.id).first()
+        #print(productoActualiza)
+        productoActualiza.activo = 0
+        database.session.commit()
+        return 1
+
 
 
     def get_by_id(id):
