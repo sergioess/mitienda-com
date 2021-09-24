f359ab67070bf0933bf6c5847d7d627f76066695
perikles <perikles7711@gmail.com>
2021-09-23 22:40:28 -0500

    actualizacion crud entradas

--- routes/entrada_bp.py
+++ routes/entrada_bp.py
@@ -8,6 +8,6 @@ entrada_bp.route('/', methods=['GET'])(index)
 entrada_bp.route('/create', methods=['GET'])(create)
 entrada_bp.route('/store', methods=['POST'])(store)
 entrada_bp.route('/<int:entrada_id>', methods=['GET'])(show)
-entrada_bp.route('/update/<int:entrada_id>', methods=['PUT'])(update)
+entrada_bp.route('/update', methods=['POST'])(update)
 entrada_bp.route('/destroy/<int:entrada_id>',
                    methods=['GET', 'POST', 'DELETE'])(destroy)
